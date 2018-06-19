c9ca80b1dc96ec9214a2bc25cbd7065c6447c659
ajayrabadiya <40312986+ajayrabadiya@users.noreply.github.com>
2018-06-19 18:19:53 +0530

    Add files via upload

---
+++ src/views/content.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Content</title>
+
+    </head>
+    <body>
+        <h1 align="center">Hello</h1>
+        <h1  align="center" style="color: #4e3ea0;">Mr. Ajay Rabadiya</h1>
+    </body>
+</html>
